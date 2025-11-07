eece3892872db5a042b05e5873d87344a52962d9
Michael Marx <michaelmarx@PandaBook.local>
2025-11-07 15:25:39 +0100

    „done“

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from stats import *
+import sys
 
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-
-def main(path):
+    path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     print(get_num_words(path))
+    print("--------- Character Count -------")
+    for key, value in get_character_count(path).items():
+        print(f"{key}: {value}")
+    #print(get_character_count(path))
+    print("============= END ===============")
 
-main("books/frankenstein.txt")
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -3,6 +3,19 @@ def get_book_text(path_to_file):
         # f is a file object, so:
         file_contents = f.read()
         return file_contents
+
+def get_character_count(path_to_file):
+    
+    character_dict = {}
+    text = get_book_text(path_to_file).lower()
+    for char in text:
+        if char in character_dict:
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+    #return character_dict
+    return dict(sorted(character_dict.items(), key=lambda item: item[1], reverse=True))
     
 def get_num_words(path_to_file):
+    
     return f"Found {len(get_book_text(path_to_file).split())} total words"
